455b77a4d7134448f98b32e941dd751b3768e4d8
Carlos Felipe Araujo <carlosxfelipe@gmail.com>
2024-11-03 08:42:03 -0300

    image optimizations

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,18 +2,21 @@ import { defineCollection, z } from "astro:content";
 
 const blogCollection = defineCollection({
   type: "content",
-  schema: z.object({
-    title: z.string(),
-    date: z.date(),
-    description: z.string(),
-    image: z.string(),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      date: z.date(),
+      description: z.string(),
+      image: image().refine((img) => img.width < 1200, {
+        message: "Image should be lower than 1200px",
+      }),
 
-    // Relação
-    author: z.string(),
+      // Relação
+      author: z.string(),
 
-    // Relação
-    tags: z.array(z.string()),
-  }),
+      // Relação
+      tags: z.array(z.string()),
+    }),
 });
 
 export const collections = { blog: blogCollection };

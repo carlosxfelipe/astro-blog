99b0334e3b29cf6e4251c27bc590379631839c7e
Carlos Felipe Araujo <carlosxfelipe@gmail.com>
2024-11-29 22:16:20 -0300

    author data

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,4 +1,4 @@
-import { defineCollection, z } from "astro:content";
+import { defineCollection, reference, z } from "astro:content";
 
 const blogCollection = defineCollection({
   type: "content",
@@ -12,11 +12,21 @@ const blogCollection = defineCollection({
       }),
 
       // Relação
-      author: z.string(),
+      // author: z.string(),
+      author: reference("author"),
 
       // Relação
       tags: z.array(z.string()),
     }),
 });
 
-export const collections = { blog: blogCollection };
+const authorCollection = defineCollection({
+  type: "data",
+  schema: ({ image }) =>
+    z.object({
+      name: z.string(),
+      avatar: image(),
+    }),
+});
+
+export const collections = { blog: blogCollection, author: authorCollection };
